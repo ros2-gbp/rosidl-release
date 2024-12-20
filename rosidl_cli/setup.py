dadeae6558d199851c12121048864e6cde5bfa9d
Chris Lalancette <clalancette@gmail.com>
2024-12-20 16:00:40 +0000

    Rebase from 'upstream'

--- rosidl_cli/setup.py
+++ rosidl_cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='rosidl_cli',
-    version='4.9.2',
+    version='4.9.3',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],
