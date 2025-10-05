ed6adb587b9a1229372183c8644fef5d3789eab3
Christophe Bedard <bedard.christophe@gmail.com>
2025-10-05 15:46:10 -0700

    Rebase from 'upstream'

--- rosidl_pycommon/setup.py
+++ rosidl_pycommon/setup.py
@@ -5,13 +5,14 @@ package_name = 'rosidl_pycommon'
 
 setup(
     name=package_name,
-    version='5.0.1',
+    version='5.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
+    package_data={'': ['py.typed']},
     install_requires=['setuptools'],
     zip_safe=True,
     author='Jacob Perron',
