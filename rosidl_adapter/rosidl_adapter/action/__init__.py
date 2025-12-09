8a683d298343e264ad321ba06003ba5173c9a86f
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-12-09 11:30:54 +0100

    Revert "Trimmed the branch to only the rosidl_pycommon sub directory"
    This reverts commit e1107b4483fb4dbb540d89db31f2216fd96e2a3e.

---
+++ rosidl_adapter/rosidl_adapter/action/__init__.py
@@ -0,0 +1,43 @@
+# Copyright 2018-2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
+
+from rosidl_adapter.parser import parse_action_string
+from rosidl_adapter.resource import ActionData, expand_template
+
+
+def convert_action_to_idl(package_dir: Path, package_name: str, input_file: Path,
+                          output_dir: Path) -> Path:
+    assert package_dir.is_absolute()
+    assert not input_file.is_absolute()
+    assert input_file.suffix == '.action'
+
+    abs_input_file = package_dir / input_file
+    print(f'Reading input file: {abs_input_file}')
+    abs_input_file = package_dir / input_file
+    content = abs_input_file.read_text(encoding='utf-8')
+    action = parse_action_string(package_name, input_file.stem, content)
+
+    output_file = output_dir / input_file.with_suffix('.idl').name
+    abs_output_file = output_file.absolute()
+    print(f'Writing output file: {abs_output_file}')
+    data: ActionData = {
+        'pkg_name': package_name,
+        'relative_input_file': input_file.as_posix(),
+        'action': action,
+    }
+
+    expand_template('action.idl.em', data, output_file, encoding='iso-8859-1')
+    return output_file
