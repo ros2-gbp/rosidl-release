f4beff28c73a7211cb59e34cd51064c9d516cfbe
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-03-24 10:57:40 +0100

    Rebase from 'release/rolling/rosidl_runtime_cpp'

--- include/rosidl_runtime_cpp/traits.hpp
+++ include/rosidl_runtime_cpp/traits.hpp
@@ -151,10 +151,10 @@ inline void value_to_yaml(const std::u16string & value, std::ostream & out)
 }
 
 template<typename T>
-inline const char * data_type();
+constexpr const char * data_type();
 
 template<typename T>
-inline const char * name();
+constexpr const char * name();
 
 template<typename T>
 struct has_fixed_size : std::false_type {};
@@ -185,6 +185,9 @@ struct is_action_result : std::false_type {};
 
 template<typename T>
 struct is_action_feedback : std::false_type {};
+
+template<typename T>
+struct MessageTraits {};
 
 }  // namespace rosidl_generator_traits
 
