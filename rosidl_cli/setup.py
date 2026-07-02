5c7d87d5476ee96dc13cae98ce01f842679b54b8
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-07-02 11:02:03 +0200

    Imported upstream version '4.9.7' of 'upstream'

--- rosidl_cli/setup.py
+++ rosidl_cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='rosidl_cli',
-    version='4.6.9',
+    version='4.9.7',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],
